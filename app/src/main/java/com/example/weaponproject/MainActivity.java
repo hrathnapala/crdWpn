fa6587b5ab34201297207f80872c5d1b7438c465
hrathnapala <43751413+hrathnapala@users.noreply.github.com>
2020-06-26 16:14:05 +0530

    day2-01

--- app/src/main/java/com/example/weaponproject/MainActivity.java
+++ app/src/main/java/com/example/weaponproject/MainActivity.java
@@ -5,16 +5,14 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,60 +22,53 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
     ImageView imageView;
-    Button button,reset,calc;
+    Button reset, calc,selectImage;
+    private final int   IMG_REQUEST = 1;
     EditText editText;
     TextView textView;
-    int x,y;
+    float scale = 0;
+    int x, y;
     ArrayList<Integer> coordinates;
     ArrayList<Double> calculation;
     double i = 0;
     Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.answer);
         editText = findViewById(R.id.unit);
+        selectImage = findViewById(R.id.button2);
         calc = findViewById(R.id.calc);
-        button = findViewById(R.id.btn);
-        imageView = findViewById(R.id.imageView1) ;
+        imageView = findViewById(R.id.imageView1);
         final ImageView iv = new ImageView(MainActivity.this);
         coordinates = new ArrayList();
         calculation = new ArrayList();
         reset = findViewById(R.id.reset);
         final RelativeLayout rl = (RelativeLayout) findViewById(R.id.relativeLayout);
-        //final RelativeLayout rl = (RelativeLayout) findViewById(R.id.relativeLayout);
-//        final PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
-//        photoView.setImageResource(R.drawable.ic_launcher_background);
-//        photoView.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
-//                x = (int) motionEvent.getX();
-//                y = (int) motionEvent.getY();
-//                int[] viewCoords = new int[2];
-//                int touchX = (int) motionEvent.getX();
-//                int touchY = (int) motionEvent.getY();
-//
-//                int imageX = touchX - viewCoords[0]; // viewCoords[0] is the X coordinate
-//                int imageY = touchY - viewCoords[1]; // viewCoords[1] is the y coordinate
-//                photoView.getLocationOnScreen(viewCoords);
-//
-//                coordinates.add(x);
-//                coordinates.add(y);
-//                System.out.println(x + " " + y);
-//                return false;
-//            }
-//        });
+
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imageView.setVisibility(View.INVISIBLE);
+                //imageView.setVisibility(View.INVISIBLE);
+                editText.setText("");
                 calculation.clear();
                 coordinates.clear();
                 textView.setVisibility(View.GONE);
@@ -92,45 +83,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         Button photoButton = (Button) this.findViewById(R.id.button1);
-        photoButton.setOnClickListener(new View.OnClickListener()
-        {
+        photoButton.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
 
-                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
 
             }
         });
 
-        imageView.setOnTouchListener(new View.OnTouchListener() {
+        PhotoViewAttacher photoView = new PhotoViewAttacher(imageView);
+        photoView.update();
+        photoView.setOnScaleChangeListener(new PhotoViewAttacher.OnScaleChangeListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
+            public void onScaleChange(float scaleFactor, float focusX, float focusY) {
+                scale = scaleFactor;
+                System.out.println(scaleFactor);
+            }
+        });
+
+        photoView.setOnViewTapListener(new PhotoViewAttacher.OnViewTapListener() {
+            @Override
+            public void onViewTap(View view, float x, float y) {
                 iv.setVisibility(View.VISIBLE);
                 textView.setVisibility(View.GONE);
-                if(calculation.size() == 8){
+                if (calculation.size() == 8) {
                     Toast.makeText(MainActivity.this, "Sorry", Toast.LENGTH_SHORT).show();
-                }else {
-                    System.out.println(coordinates.size());
-                    x = (int) motionEvent.getX();
-                    y = (int) motionEvent.getY();
-                    int[] viewCoords = new int[2];
-                    int touchX = (int) motionEvent.getX();
-                    int touchY = (int) motionEvent.getY();
-
-                    int imageX = touchX - viewCoords[0]; // viewCoords[0] is the X coordinate
-                    int imageY = touchY - viewCoords[1]; // viewCoords[1] is the y coordinate
-                    imageView.getLocationOnScreen(viewCoords);
-                    coordinates.add(x);
-                    coordinates.add(y);
-
-                    calculation.add((double) x);
-                    calculation.add((double) y);
+                } else {
+                    if ((int) scale == 0) {
+                        scale = 1;
+                    }
+                    calculation.add((double) x / scale);
+                    calculation.add((double) y / scale);
 
                     System.out.println(x + " " + y);
-
 
 
                     Bitmap bm = BitmapFactory.decodeResource(getResources(),
@@ -139,8 +127,8 @@ public class MainActivity extends AppCompatActivity {
                     RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
                             RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 
-                    params.leftMargin = x - 65;
-                    params.topMargin = y + 58;
+                    params.leftMargin = (int) (x - 65);
+                    params.topMargin = (int) (y + 60);
 
 
                     if (iv.getParent() != null) {
@@ -149,31 +137,19 @@ public class MainActivity extends AppCompatActivity {
 
                     rl.addView(iv, params);
                 }
-
-                return false;
-            }
-        });
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    double b = getScreenDistance(coordinates.get(0),coordinates.get(1),coordinates.get(2),coordinates.get(3));
-                    calculation.add(b);
-                    System.out.println(b*2.54);
-                    textView.setText("" + b*2.54);
-                    textView.setVisibility(View.VISIBLE);
-                    coordinates.clear();
-                    iv.setVisibility(View.GONE);
-                }catch (Exception e){
-                    Toast.makeText(MainActivity.this, ",", Toast.LENGTH_SHORT).show();
-                }
-
-
             }
         });
 
     }
-    public void calculate(){
+
+    public void selectImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,IMG_REQUEST);
+    }
+
+    public void calculate() {
         try {
             double a = calculation.get(0);
             double b = calculation.get(1);
@@ -184,20 +160,30 @@ public class MainActivity extends AppCompatActivity {
             double g = calculation.get(6);
             double h = calculation.get(7);
 
-            double answer1 = Math.sqrt(Math.pow((a-c),2) + Math.pow((b-d),2) );
-            double answer2 = Math.sqrt(Math.pow((e-g),2) + Math.pow((f-h),2) );
+            double answer1 = Math.sqrt(Math.pow((a - c), 2) + Math.pow((b - d), 2));
+            double answer2 = Math.sqrt(Math.pow((e - g), 2) + Math.pow((f - h), 2));
             double answer3 = Double.parseDouble(editText.getText().toString());
-            double answer4 = ((answer3/answer1)*answer2);
+            double answer4 = ((answer3 / answer1) * answer2);
             calculation.clear();
             editText.setText("");
             textView.setText("" + answer4);
             textView.setVisibility(View.VISIBLE);
 
-        }catch (Exception e){
+            double p1 = -5.995;
+            double p2 = 829.5;
+            double p3 = -5554;
+            double q1 = -1.69;
+            double q2 = -40.59;
+
+            double answer5 = (p1 * Math.pow(answer4,2) + p2 * answer4 + p3) / (Math.pow(answer4,2) - q1 * answer4 + q2);
+            editText.setText(""+answer5);
+
+        } catch (Exception e) {
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
         }
 
     }
+
     public double getScreenDistance(double x1, double y1, double x2, double y2) {
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -205,11 +191,11 @@ public class MainActivity extends AppCompatActivity {
         double yDist = Math.pow(Math.abs(y1 - y2) / dm.ydpi, 2);
         return Math.sqrt(xDist + yDist);
     }
+
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        int x = (int)event.getX();
-        int y = (int)event.getY();
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
@@ -219,17 +205,32 @@ public class MainActivity extends AppCompatActivity {
 
         return false;
     }
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
+        if(requestCode == IMG_REQUEST && resultCode == RESULT_OK && data != null){
+            Uri path = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),path);
+                Bitmap resized = Bitmap.createScaledBitmap(bitmap,330, 330, true);
+                imageView.setImageBitmap(resized);
+                imageView.setVisibility(View.VISIBLE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
+            imageView.setVisibility(View.VISIBLE);
+        }
+
+
 
         super.onActivityResult(requestCode, resultCode, data);
     }
